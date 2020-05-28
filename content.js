531a1f4378a37378800d4589b87615bce9c10400
Sebastian Landwehr <sebastianlandw@gmail.com>
2020-05-28 20:26:23 +0200

    fix: upgrades

--- content.js
+++ content.js
@@ -1,61 +1,43 @@
-import $ from 'jquery'
-import shieldman from 'shieldman'
-import GitHub from 'github-api'
+import { property } from '@dword-design/functions'
+import cheerio from 'cheerio'
+import axios from 'axios'
 
 const token = '0c6062e4e326c10ced6d3a8ad34ac66ab2b8a8fa'
 
-const github = new GitHub({ token })
-
 const userName = window.location.href.match(/github\.com\/(.*?)\?/)[1]
-const $containers = $('#user-repositories-list h3').parent().parent()
+const github = axios.create({ baseURL: 'https://api.github.com' })
+const $headlines = document.querySelectorAll('#user-repositories-list h3')
 
-$containers.each(function () {
-  ;(async () => {
-    const $headlineContainer = $(this).find('h3').parent()
-    const repositoryName = $headlineContainer.find('a').text().trim()
-    const repository = github.getRepo(userName, repositoryName)
+$headlines.forEach(async $headline => {
+  const $headlineContainer = $headline.parentNode
+  const $repository = $headlineContainer.parentNode
+  const repositoryName = $headline.querySelector('a').textContent.trim()
 
-    try {
-      const [{ name: packageName }, readme] = await Promise.all([
-        repository
-          .getContents(undefined, 'package.json')
-          .then(({ data: { content, encoding } }) =>
-            JSON.parse(Buffer.from(content, encoding).toString())
-          ),
-        repository
-          .getReadme()
-          .then(({ data: { content, encoding } }) =>
-            Buffer.from(content, encoding).toString()
-          ),
-      ])
-
-      const shieldMatch = readme.match(/<!--@shields\((.*)\)-->/) || undefined
-      const shieldNames =
-        shieldMatch !== undefined
-          ? shieldMatch[1]
-              .split(/,\s*/)
-              .map(part => part.substr(1, part.length - 2))
-          : []
-
-      const shieldsHtml = shieldNames
-        .map(shieldName =>
-          shieldman(shieldName, {
-            npmName: packageName,
-            repo: `${userName}/${repositoryName}`,
-            branch: 'master',
-          })
-        )
-        .map(
-          ({ link, text, image }) =>
-            `<a href="${link}"><img alt="${text}" src="${image}"></a>`
-        )
-        .join(' ')
-
-      $(`<div>${shieldsHtml}</div>`).insertAfter($headlineContainer)
-    } catch (e) {
-      if ((e.response || {}).status !== 404) {
-        throw e
-      }
+  try {
+    const readme =
+      github.get(`/repos/${userName}/${repositoryName}/readme`, {
+        headers: {
+          Authorization: `token ${token}`,
+          Accept: 'application/vnd.github.v3.html',
+        },
+      })
+      |> await
+      |> property('data')
+    let $badges = $repository.querySelector(
+      '.github-better-repository-list-badges'
+    )
+    if (!$badges) {
+      $badges = document.createElement('div')
+      $badges.classList.add('github-better-repository-list-badges')
+      $headlineContainer.after($badges)
     }
-  })()
+    const $ = cheerio.load(readme)
+    $badges.innerHTML = $('[data-canonical-src^="https://img.shields.io"]')
+      .parent()
+      .map((index, badge) => $(badge).html())
+      .get()
+      .join('\n')
+  } catch (error) {
+    console.error(error)
+  }
 })
