0d0b36f0d29b05c54b2be42625e8f9c19a5a5e99
Sebastian Landwehr <info@sebastianlandwehr.com>
2021-06-21 11:53:08 +0200

    fix: tests (#33)

---
+++ index.spec.js
@@ -0,0 +1,32 @@
+import tester from '@dword-design/tester'
+import testerPluginPuppeteer from '@dword-design/tester-plugin-puppeteer'
+import execa from 'execa'
+import P from 'path'
+
+export default tester(
+  {
+    async works() {
+      await this.page.goto(
+        'https://github.com/github-repository-list-badges?tab=repositories',
+        { waitUntil: 'networkidle0' }
+      )
+
+      const repositoriesList = await this.page.waitForSelector(
+        '#user-repositories-list'
+      )
+      expect(await repositoriesList.screenshot()).toMatchImageSnapshot(this)
+    },
+  },
+  [
+    { before: () => execa.command('base prepublishOnly') },
+    testerPluginPuppeteer({
+      launchOptions: {
+        args: [
+          `--load-extension=${P.join(process.cwd(), 'dist')}`,
+          `--disable-extensions-except=${P.join(process.cwd(), 'dist')}`,
+        ],
+        headless: false,
+      },
+    }),
+  ]
+)
